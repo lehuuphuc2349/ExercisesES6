4819bb65a893cbbe622a78f8a42fbc466f9d8bea
lehuuphuc2349 <57623623+lehuuphuc2349@users.noreply.github.com>
2020-05-16 13:05:28 +0700

    commit

---
+++ part1/csvObjtoString.js
@@ -0,0 +1,17 @@
+const csv_to_array = (arr, columns, delimiter = ",") =>
+  [
+    columns.join(delimiter),
+    ...arr.map((obj) =>
+      columns.reduce(
+        (acc, key) =>
+          `${acc}${!acc.length ? "" : delimiter}"${!obj[key] ? "" : obj[key]}"`,
+        ""
+      )
+    ),
+  ].join("\n");
+console.log(
+  csv_to_array(
+    [{ x: 100, y: 200 }, { x: 300, y: 400, z: 500 }, { x: 6 }, { y: 7 }],
+    ["x", "y"]
+  )
+);
