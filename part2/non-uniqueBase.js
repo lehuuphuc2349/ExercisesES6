3fda2cd653b124b6461fdc0f9c096905ef67e523
lehuuphuc2349 <57623623+lehuuphuc2349@users.noreply.github.com>
2020-05-17 09:33:40 +0700

    commit

---
+++ part2/non-uniqueBase.js
@@ -0,0 +1,14 @@
+const nonUniqueBased = (array, fn) =>
+  array.filter((v, i) => array.every((x, j) => (i === j) === fn(v, x, i, j)));
+console.log(
+  nonUniqueBased(
+    [
+      { id: 0, value: "a" },
+      { id: 1, value: "b" },
+      { id: 2, value: "c" },
+      { id: 1, value: "d" },
+      { id: 0, value: "e" },
+    ],
+    (a, b) => a.id == b.id
+  )
+);
