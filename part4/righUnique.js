cf1f518afe2f63c23ed913586e31d8824ea1251b
lehuuphuc2349 <57623623+lehuuphuc2349@users.noreply.github.com>
2020-06-26 10:07:33 +0700

    commit

--- part4/righUnique.js
+++ part4/righUnique.js
@@ -18,3 +18,31 @@ console.log(
     (a, b) => a.id == b.id
   )
 );
+const value = (arr) => {
+  let result = [];
+  let max = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 == 0) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+};
+console.log(value([1, 2, 3, 4]));
+const findMaxValue = (arr) => {
+  let max = Math.max(...arr);
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] == max) {
+      return arr[i];
+    }
+  }
+};
+console.log(findMaxValue([10, 12, 3, 311]));
+const unique2Array = (arr1, arr2) => {
+  let result = [];
+  if (arr1.length !== 0 && arr2.length !== 0) {
+    result = new Set([...arr1, ...arr2]);
+  }
+  return result;
+};
+console.log(unique2Array([1, 2, 3, 4, 5], [4, 5, 6, 7, 8]));
